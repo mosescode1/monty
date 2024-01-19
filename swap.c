f2419d673f63249da36f6372dda2f3cfff1464d2
mosescode1 <emosescode@gmail.com>
2024-01-19 16:24:46 +0100

    fixed swap

--- swap.c
+++ swap.c
@@ -9,18 +9,17 @@ void swap(stack_t **stack, unsigned int code_line_number)
 	stack_t *s1, *s2;
 	int temp, temp2;
 
-	s1 = *stack;
-	s2 = (*stack)->next;
-	if (s1 != NULL && s2 != NULL)
-	{
-		temp = s1->n;
-		temp2 = s2->n;
-		s2->n = temp;
-		s1->n = temp2;
-	}
-	else
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", code_line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	s1 = *stack;
+	s2 = (*stack)->next;
+
+	temp = s1->n;
+	temp2 = s2->n;
+	s2->n = temp;
+	s1->n = temp2;
 }
--- swap.m
+++ swap.m
@@ -1,7 +1,1 @@
-push 1
-push 2
-push 3
-pint
 swap
-pop
-pall
